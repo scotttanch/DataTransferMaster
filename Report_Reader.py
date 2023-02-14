56b8c2e7d08b7e86c62dd97403f2f2fc27c03fac
scotttanch <stanch@uvm.edu>
2023-02-14 11:45:55 -0500

    added support for my new "feature"

--- Report_Reader.py
+++ Report_Reader.py
@@ -4,28 +4,35 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta as td
 import pandas as pd
+import os
 
 # Location of the report files and the stack containing them
 report_folder = "C:\\Users\\Scott\\Documents\\GitHub\\DataTransferMaster\\reports\\"
+#report_folder = "C:\\Users\\Scott\\Documents\\GitHub\\DataTransferMaster\\reports\\Test 2 1-26 2-7\\"
 file_paths = tls.full_stack(report_folder,"txt")
+delimiter = os.sep
+
 
 # Session report class definition
 class Session_Report:
     def __init__(self,file_path):
-        self.name = file_path.split('\\')[-1].split(".")[0]
+        self.name = file_path.split(delimiter)[-1].split(".")[0]
         self.creation_date = self.name.split('_')[1]
         self.creation_time = self.name.split('_')[2]
         self.time_string = self.creation_date+"-2022::"+self.creation_time
         self.creation_date = datetime.strptime(self.time_string,"%m-%d-%Y::%H-%M")
         with open(self.name+'.txt', 'r') as f:
             self.data = f.readlines()[1:]
-
+        
+        if self.data[-1].split(',')[0] == 'File':       # I accidently made the reports write the column labels on the top and bottom
+            self.data = self.data[1:-1]                 # I'm calling it a feature not a bug, this deals with that feature
+                                                   
         tmp = []
         for line in self.data:
             val = float(line.split(',')[4])
             tmp.append(val)
-        self.avg_Bit = np.mean(tmp)*1000          # the *1000 stays until i push the new code out that deals with the 
-        self.std_Bit = np.std(tmp)*1000           # fact that i divided by 1000 one too many times
+        self.avg_Bit = np.mean(tmp)#*1000          # the *1000 stays until i push the new code out that deals with the 
+        self.std_Bit = np.std(tmp)#*1000           # fact that i divided by 1000 one too many times, Applys to reports created before 2/7/23
 
         tmp = []
         for line in self.data:
@@ -73,14 +80,16 @@ for report in reports:
 ticks = pd.date_range(min(Dates),max(Dates),6)
 labels = []
 for date in ticks:
-    string = str(date.month) + "/" + str(date.day) + " " + str(date.hour) + ":" + str(date.minute)
+    string = str(date.month) + "/" + str(date.day) + " " + str(date.hour).zfill(2) + ":" + str(date.minute).zfill(2)
     labels.append(string)
 
 # Print some values
 avg_sending = np.mean(S_Times)
 avg_processing = np.mean(P_Times)
-print("Average time to send: " + str(avg_sending))
-print("Average Processing time: " + str(avg_processing))
+avg_bit = np.mean(Bit_rates)
+print("Average Time to send: " + str(round(avg_sending,3)) + " (s)")
+print("Average Processing time: " + str(round(avg_processing,3)) +" (s)" )
+print("Average Speed: " + str(round(avg_bit,3)) + " (Mb/s)")
 
 # Response Time subplot
 plt.subplot(2,1,2)
